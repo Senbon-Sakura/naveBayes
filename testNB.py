cdad4a287bc92fe1d2da2098a871a2082baa9386
Senbon-Sakura <wangzongwu@outlook.coom>
2020-05-23 23:41:29 +0800

    add car_data, and update function with 10-fold verification

--- testNB.py
+++ testNB.py
@@ -29,57 +29,60 @@ def createDataSet(filename):
         dataArr.append(rowVals[1:-1])
         labelArr.append(rowVals[-1])
 
-    print(dataArr)
-    print(labelArr)
     return np.array(dataArr), np.array(labelArr)
+
+
+def splitDataSet(dataArr, labelArr, K=10, k=0):
+    trainingData = [x for i, x in enumerate(dataArr) if i % K != k]
+    trainingLabel = [x for i, x in enumerate(labelArr) if i % K != k]
+    testData = [x for i, x in enumerate(dataArr) if i % K == k]
+    testLabel = [x for i, x in enumerate(labelArr) if i % K == k]
+    trainingData = np.array(trainingData)
+    trainingLabel = np.array(trainingLabel)
+    testData = np.array(testData)
+    testLabel = np.array(testLabel)
+    return trainingData, trainingLabel, testData, testLabel
 
 def NaiveBayes(dataArr, labelArr, testVec):
     numSamp = np.shape(dataArr)[0]
     numFeat = np.shape(dataArr)[1]
-    print("Sample num is: ", numSamp)
-    print("Feature num is: ", numFeat)
+    #print("Sample num is: ", numSamp)
+    #print("Feature num is: ", numFeat)
     # 计算先验概率P(ci)
     #labelN = len(set(labelArr))
+    labelUniq = np.unique(labelArr)
     labelN = len(np.unique(labelArr))
-    PC1 = (np.sum(labelArr == 1.0)+1)/(numSamp + labelN)        # laplace平滑
-    PC0 = (np.sum(labelArr == 0.0)+1)/(numSamp + labelN)        # laplace平滑
-    print("PC0 = ", PC0)
-    print("PC1 = ", PC1)
+    PC = []         # 保存PC值列表
+    for i in range(labelN):
+        PC.append( (np.sum(labelArr == labelUniq[i])+1) / (numSamp + labelN) )
+    PC = np.array(PC)
+    #print(PC)
+    Pcond_Feat = np.zeros(labelN)
+    Pcond = np.ones_like(labelN)
 
-    PcondGood = 1
-    PcondBad = 1
     # 对每一个xi，计算类条件概率P(xi|cj)，然后累乘得到P(x|cj)
     # 先计算P(xi|c1)，并将结果保存到PcondGood列表中
     for i in range(numFeat):
-        indexGood = np.where(labelArr == 1.0)
-        attrN = len(np.unique(dataArr[:, i]))
-        if attrN < 5:               # 离散值使用大数定理进行估计
-            PcondGood_i = (np.sum(dataArr[indexGood, i] == testVec[i])+1)/(np.shape(indexGood)[1] + attrN)    # laplace平滑
-        else:                       # 连续值先假设服从高斯分布，然后采用极大似然估计
-            Mean_ci = np.mean(dataArr[indexGood, i])
-            Var_ci = np.var(dataArr[indexGood, i], ddof=1)
-            PcondGood_i = (1 / (np.sqrt(2 * np.pi * Var_ci)) * np.exp(-(testVec[i] - Mean_ci) ** 2 / (2 * Var_ci)))
-        PcondGood *= PcondGood_i
-
-    # 先计算P(xi|c0)，并将结果保存到PcondBad
-    for i in range(numFeat):
-        indexBad = np.where(labelArr == 0.0)
-        attrN = len(np.unique(dataArr[:, i]))
-        if attrN < 5:               # 离散值使用大数定理进行估计
+        for j in range(labelN):
+            indexClass = np.where(labelArr == labelUniq[j])
             attrN = len(np.unique(dataArr[:, i]))
-            PcondBad_i = (np.sum(dataArr[indexBad, i] == testVec[i]) + 1)/(np.shape(indexBad)[1] + attrN)
-        else:                       # 连续值先假设服从高斯分布，然后采用极大似然估计
-            Mean_ci = np.mean(dataArr[indexGood, i])
-            Var_ci = np.var(dataArr[indexGood, i], ddof=1)
-            PcondBad_i = (1 / (np.sqrt(2 * np.pi* Var_ci) ) * np.exp(-(testVec[i] - Mean_ci) ** 2 / (2 * Var_ci)))
-        PcondBad *= PcondBad_i
+            if attrN < 5:               # 离散值使用大数定理进行估计
+                #print(np.sum(dataArr[indexClass, i] == testVec[i]))
+                #print(len(indexClass[0]))
+                Pcond_Feat[j] = (np.sum(dataArr[indexClass, i] == testVec[i])+1)/(len(indexClass[0]) + attrN)    # laplace平滑
+            else:                       # 连续值先假设服从高斯分布，然后采用极大似然估计
+                Mean_cij = np.mean(dataArr[indexClass, i])
+                Var_cij = np.var(dataArr[indexClass, i], ddof=1)
+                Pcond_Feat[j] = (1 / (np.sqrt(2 * np.pi * Var_cij)) * np.exp(-(testVec[i] - Mean_cij) ** 2 / (2 * Var_cij)))
+        Pcond = np.multiply(Pcond, Pcond_Feat)
+
 
     # 计算联合概率P(x|ci)
-    PBad = PcondBad*PC0
-    PGood = PcondGood*PC1
-
-    print("PBad = ", PBad)
-    print("PGood = ", PGood)
+    Puni = Pcond*PC
+    #print("Puni = ", Puni)
+    indBest = np.argmax(Puni)
+    #print("The answer is: ", labelUniq[indBest])
+    return labelUniq[indBest]
 
 #色泽 1-3代表 浅白 青绿 乌黑
 #根蒂 1-3代表 稍蜷 蜷缩 硬挺
@@ -90,10 +93,32 @@ def NaiveBayes(dataArr, labelArr, testVec):
 #好瓜 1代表 是 0 代表 不是
 #dataArr, labelArr = createDataSet('西瓜数据集3.0.xlsx')
 #testVec = [2, 2, 1, 1, 3, 1, 0.697, 0.460]      # 敲声为清脆，需要用到laplace平滑
-dataArr, labelArr = createDataSet('婚姻分析.xlsx')
-testVec1 = [0, 0, 0, 0]
-testVec2 = [1, 1, 2, 1]
+#dataArr, labelArr = createDataSet('婚姻分析.xlsx')
+#testVec = [1, 1, 2, 1]      # 敲声为清脆，需要用到laplace平滑
+'''
+| class values
+unacc, acc, good, vgood
+| attributes
+buying:   vhigh, high, med, low.
+maint:    vhigh, high, med, low.
+doors:    2, 3, 4, 5more.
+persons:  2, 4, more.
+lug_boot: small, med, big.
+safety:   low, med, high.
+'''
+dataArr, labelArr = createDataSet('./car_data/car.xlsx')
+K = 10  # 10-fold verification
+for k in range(K):
+    trainingData, trainingLabel, testData, testLabel = splitDataSet(dataArr, labelArr, K, k)
+    numTest = len(testLabel)
+    numRight = 0
+    for i in range(len(testLabel)):
+        testVec = testData[i]
+        classReturn = NaiveBayes(trainingData, trainingLabel, testVec)
+        if (classReturn == testLabel[i]):
+            numRight += 1
+    accuracy = numRight / numTest
+    print(k, "-th Classification accuracy is: ", accuracy)
 
-NaiveBayes(dataArr, labelArr, testVec1)
-NaiveBayes(dataArr, labelArr, testVec2)
+
 
